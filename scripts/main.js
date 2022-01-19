51ed8bfb70e9e439ac51d8bb21e2b0ca4a68ceb6
srujan <srujan.sharma77k@gmail.com>
2022-01-19 16:44:36 +1100

    dark mode prototype

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+var body = document.body;
+var btn = document.getElementById("toggle");
+btn.addEventListener('click', function () {
+    body.classList.toggle('light-mode');
+})
+
+// var btn = document.getElementById("toggle");
+// btn.addEventListener('click', function () {
+//     document.body.classList.toggle('light-mode');
+// })
+
+console.log('everything is loaded');
