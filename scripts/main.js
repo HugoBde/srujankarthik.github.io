dd7b52c56f403ee3d0119d8385b5648725deee65
srujan <srujan.sharma77k@gmail.com>
2022-01-20 19:45:45 +1100

    fixed icons for toggle

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
 let body = document.body;
 let btn = document.getElementById("toggle-view");
+// let darkMode = document.querySelector('.bi bi-moon-fill');
 btn.addEventListener('click', function () {
     body.classList.toggle('light-mode');
+    btn.classList.toggle('bi-moon-fill');
+    btn.classList.toggle('bi-brightness-high');
 })
 
 console.log('everything is loaded');
